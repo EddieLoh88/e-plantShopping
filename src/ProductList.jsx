a4f98a4ca83a2589545a8a670896ec81bf1a12d4
EddieLoh88 <elf604@hotmail.com>
2024-12-26 20:48:31 -0500

    initial commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,11 +1,19 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+
+
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-    const [addedToCart, setAddedToCart] = useState({});
-   //
+    const [addedToCart, setAddedToCart] = useState({}); // To track which products are added to cart
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+           ...prevState,
+           [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+         }));
+      };
     const plantsArray = [
         {
             category: "Air Purifying Plants",
